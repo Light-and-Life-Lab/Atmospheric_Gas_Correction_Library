ca21f847fae169ba1760838fb48209848fdff9aa
Timothy Kindervatter <timothy@picon.kobol.net>
2025-07-24 11:55:11 -0400

    Added py::array::c_style to all input parameters, which fixed bug where k_oz was not interpreted correctly upon being passed into C++ (row vs column major memory layout issue). Also changed all cases of solar and sensor zenith angles to angle cosines.

--- gas_transmittance.cpp
+++ gas_transmittance.cpp
@@ -1,5 +1,5 @@
 <%
-cfg['compiler_args'] = ['-std=c++17']
+cfg['compiler_args'] = ['-std=c++17', '-O3']
 setup_pybind11(cfg)
 %>
 
@@ -14,16 +14,17 @@ setup_pybind11(cfg)
 
 namespace py = pybind11;
 
-std::tuple<py::array_t<double>, py::array_t<double>, py::array_t<double>> ozone_transmittance(py::array_t<double> k_oz, 
-                         py::array_t<double> l1b_oz, 
-                         py::array_t<double> l1b_solz, 
-                         py::array_t<double> l1b_senz,
-                         bool do_amf_correction) 
+std::tuple<py::array_t<double>, py::array_t<double>, py::array_t<double>> ozone_transmittance(
+    py::array_t<double, py::array::c_style> k_oz, 
+    py::array_t<double, py::array::c_style> l1b_oz, 
+    py::array_t<double, py::array::c_style> l1b_csolz, 
+    py::array_t<double, py::array::c_style> l1b_csenz,
+    bool do_amf_correction) 
 {
     double* k_oz_ptr = static_cast<double*>(k_oz.request().ptr);
     double* l1b_oz_ptr = static_cast<double*>(l1b_oz.request().ptr);
-    double* l1b_solz_ptr = static_cast<double*>(l1b_solz.request().ptr);
-    double* l1b_senz_ptr = static_cast<double*>(l1b_senz.request().ptr);
+    double* l1b_csolz_ptr = static_cast<double*>(l1b_csolz.request().ptr);
+    double* l1b_csenz_ptr = static_cast<double*>(l1b_csenz.request().ptr);
 
     int n_rows = l1b_oz.request().shape[0];
     int num_pixels = n_rows;
@@ -42,25 +43,22 @@ std::tuple<py::array_t<double>, py::array_t<double>, py::array_t<double>> ozone_
 
     // Input arrays have values for each pixel at each wavelength
     // If the array is considered as a 2D matrix then each row contains the values at every wavelength for a given pixel
-    py::print("Computing transmittances");
     for (int ip = 0; ip < num_pixels; ip++)
     {
-        py::print("\nip = ", ip);
         int row_offset = ip*num_wavelengths; // Each row represents a single pixel and has num_wavelengths elements
 
         for (int iw = 0; iw < num_wavelengths; iw++) 
         {
-            py::print("iw = ", iw);
             tau_oz = l1b_oz_ptr[ip] * k_oz_ptr[iw];
-            tg_sol_ptr[row_offset + iw] = exp(-(tau_oz / l1b_solz_ptr[ip]));
+            tg_sol_ptr[row_offset + iw] = exp(-(tau_oz / l1b_csolz_ptr[ip]));
 
             if (do_amf_correction) 
             {
-                tg_ptr[row_offset + iw] = exp(-tau_oz * (1.0/l1b_solz_ptr[ip] + 1.0/l1b_senz_ptr[ip]));
+                tg_ptr[row_offset + iw] = exp(-tau_oz * (1.0/l1b_csolz_ptr[ip] + 1.0/l1b_csenz_ptr[ip]));
             } 
             else 
             {
-                tg_sen_ptr[row_offset + iw] = exp(-(tau_oz / l1b_senz_ptr[ip]));
+                tg_sen_ptr[row_offset + iw] = exp(-(tau_oz / l1b_csenz_ptr[ip]));
             }
         }
     }
