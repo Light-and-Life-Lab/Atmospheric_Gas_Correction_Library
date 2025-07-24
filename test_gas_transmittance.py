afb27123f33d65d8fcaa9901bea9c031b44b2632
Timothy Kindervatter <timothy@picon.kobol.net>
2025-07-24 11:56:36 -0400

    Changed solar and sensor zenith angles to angle cosines in test script

--- test_gas_transmittance.py
+++ test_gas_transmittance.py
@@ -7,11 +7,11 @@ import gas_transmittance
 if __name__ == "__main__":
     k_oz = np.array([1, 2, 3])
     l1b_oz = np.ones(4)
-    l1b_solz = np.array([90, 60, 30, 0])
-    l1b_senz = np.array([90, 60, 30, 0])
+    l1b_csolz = np.cos(np.deg2rad(np.array([90, 60, 30, 0])))
+    l1b_csenz = np.cos(np.deg2rad(np.array([90, 60, 30, 0])))
     do_amf_correction = False
 
-    tg_sol, tg_sen, tg = gas_transmittance.ozone_transmittance(k_oz, l1b_oz, l1b_solz, l1b_senz, do_amf_correction)
+    tg_sol, tg_sen, tg = gas_transmittance.ozone_transmittance(k_oz, l1b_oz, l1b_csolz, l1b_csenz, do_amf_correction)
     print(tg_sol)
     print(tg_sen)
     print(tg)
