87f496e5c45ad88e442a4524d8f496d36c4ee576
Timothy Kindervatter <timothy@picon.kobol.net>
2025-07-25 15:52:56 -0400

    Added function to test script in order to test no2_transmittance

--- test/test_gas_transmittance.py
+++ test/test_gas_transmittance.py
@@ -16,3 +16,19 @@ def test_ozone_transmittance():
     assert np.allclose(tg_sol, tg_sol_benchmark)
     assert np.allclose(tg_sen, tg_sen_benchmark)
 
+
+def test_no2_transmittance():
+    k_no2 = np.load('test/no2_data/k_no2.npy')
+    l1b_no2_tropo = np.load('test/no2_data/no2_tropo.npy')
+    l1b_no2_strat = np.load('test/no2_data/no2_strat.npy')
+    l1b_no2_frac = np.load('test/no2_data/no2_frac.npy')
+    l1b_csolz = np.load('test/no2_data/no2_csolz.npy')
+    l1b_csenz = np.load('test/no2_data/no2_csenz.npy')
+    do_amf_correction = False
+
+    tg_sol, tg_sen, tg = gas_transmittance.no2_transmittance(k_no2, l1b_no2_frac, l1b_no2_tropo, l1b_no2_strat, l1b_csolz, l1b_csenz, do_amf_correction)
+    tg_sol_benchmark = np.load('test/no2_data/tg_sol_no2.npy')
+    tg_sen_benchmark = np.load('test/no2_data/tg_sen_no2.npy')
+
+    assert np.allclose(tg_sol, tg_sol_benchmark)
+    assert np.allclose(tg_sen, tg_sen_benchmark)
