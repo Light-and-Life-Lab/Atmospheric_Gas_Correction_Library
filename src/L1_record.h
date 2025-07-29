17b17c27da74d8e38e847ddf2a36bf33c3fce5f2
Timothy Kindervatter <timothy@picon.kobol.net>
2025-07-29 15:55:53 -0400

    Added L1_Record class to simplify calling of gas transmittance functions

---
+++ src/L1_record.h
@@ -0,0 +1,17 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+
+struct L1_Record
+{
+public:
+    L1_Record(){};
+    
+    pybind11::array_t<double, pybind11::array::c_style> k_oz{};
+    pybind11::array_t<double, pybind11::array::c_style> l1b_oz{}; 
+    pybind11::array_t<double, pybind11::array::c_style> k_no2{};
+    pybind11::array_t<double, pybind11::array::c_style> l1b_no2_frac{};
+    pybind11::array_t<double, pybind11::array::c_style> l1b_no2_tropo{};
+    pybind11::array_t<double, pybind11::array::c_style> l1b_no2_strat{};
+    pybind11::array_t<double, pybind11::array::c_style> l1b_csolz{}; 
+    pybind11::array_t<double, pybind11::array::c_style> l1b_csenz{};
+};
