dc7b094d584307a3dd7d4ef46133db4053ea6d05
Timothy Kindervatter <timothy@picon.kobol.net>
2025-07-29 16:12:31 -0400

    Added test of new ozone transmittance overload that takes a struct argument

--- test/test_gas_transmittance.py
+++ test/test_gas_transmittance.py
@@ -2,7 +2,28 @@ import numpy as np
 
 import bin.gas_transmittance as gas_transmittance
 
-def test_ozone_transmittance():
+def test_ozone_transmittance_struct():
+    k_oz = np.load('test/ozone_data/koz.npy')
+    l1b_oz = np.load('test/ozone_data/oz_concentration.npy')
+    l1b_csolz = np.load('test/ozone_data/oz_csolz.npy')
+    l1b_csenz = np.load('test/ozone_data/oz_csenz.npy')
+    do_amf_correction = False
+
+    l1_rec = gas_transmittance.L1_Record()
+    l1_rec.k_oz = k_oz
+    l1_rec.l1b_oz = l1b_oz
+    l1_rec.l1b_csolz = l1b_csolz
+    l1_rec.l1b_csenz = l1b_csenz
+
+    tg_sol, tg_sen, tg = gas_transmittance.ozone_transmittance(l1_rec, do_amf_correction)
+    tg_sol_benchmark = np.load('test/ozone_data/tg_sol_oz.npy')
+    tg_sen_benchmark = np.load('test/ozone_data/tg_sen_oz.npy')
+
+    np.testing.assert_allclose(tg_sol, tg_sol_benchmark)
+    np.testing.assert_allclose(tg_sen, tg_sen_benchmark)
+
+
+def test_ozone_transmittance_vector():
     k_oz = np.load('test/ozone_data/koz.npy')
     l1b_oz = np.load('test/ozone_data/oz_concentration.npy')
     l1b_csolz = np.load('test/ozone_data/oz_csolz.npy')
