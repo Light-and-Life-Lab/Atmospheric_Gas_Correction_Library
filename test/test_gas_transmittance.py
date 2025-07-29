27aa1c5e5d1369d39ade832ec92e05ab47d8db50
Timothy Kindervatter <timothy@picon.kobol.net>
2025-07-29 18:22:42 -0400

    Added test for version of no2 transmittance function that takes L1_record

--- test/test_gas_transmittance.py
+++ test/test_gas_transmittance.py
@@ -38,7 +38,7 @@ def test_ozone_transmittance_vector():
     np.testing.assert_allclose(tg_sen, tg_sen_benchmark)
 
 
-def test_no2_transmittance():
+def test_no2_transmittance_vector():
     k_no2 = np.load('test/no2_data/k_no2.npy')
     l1b_no2_tropo = np.load('test/no2_data/no2_tropo.npy')
     l1b_no2_strat = np.load('test/no2_data/no2_strat.npy')
@@ -48,6 +48,31 @@ def test_no2_transmittance():
     do_amf_correction = False
 
     tg_sol, tg_sen, tg = gas_transmittance.no2_transmittance(k_no2, l1b_no2_frac, l1b_no2_tropo, l1b_no2_strat, l1b_csolz, l1b_csenz, do_amf_correction)
+    tg_sol_benchmark = np.load('test/no2_data/tg_sol_no2.npy')
+    tg_sen_benchmark = np.load('test/no2_data/tg_sen_no2.npy')
+
+    np.testing.assert_allclose(tg_sol, tg_sol_benchmark)
+    np.testing.assert_allclose(tg_sen, tg_sen_benchmark)
+
+
+def test_no2_transmittance():
+    k_no2 = np.load('test/no2_data/k_no2.npy')
+    l1b_no2_tropo = np.load('test/no2_data/no2_tropo.npy')
+    l1b_no2_strat = np.load('test/no2_data/no2_strat.npy')
+    l1b_no2_frac = np.load('test/no2_data/no2_frac.npy')
+    l1b_csolz = np.load('test/no2_data/no2_csolz.npy')
+    l1b_csenz = np.load('test/no2_data/no2_csenz.npy')
+    do_amf_correction = False
+
+    l1_rec = gas_transmittance.L1_Record()
+    l1_rec.k_no2 = k_no2
+    l1_rec.l1b_no2_frac = l1b_no2_frac
+    l1_rec.l1b_no2_tropo = l1b_no2_tropo
+    l1_rec.l1b_no2_strat = l1b_no2_strat
+    l1_rec.l1b_csolz = l1b_csolz
+    l1_rec.l1b_csenz = l1b_csenz
+
+    tg_sol, tg_sen, tg = gas_transmittance.no2_transmittance(l1_rec, do_amf_correction)
     tg_sol_benchmark = np.load('test/no2_data/tg_sol_no2.npy')
     tg_sen_benchmark = np.load('test/no2_data/tg_sen_no2.npy')
 
