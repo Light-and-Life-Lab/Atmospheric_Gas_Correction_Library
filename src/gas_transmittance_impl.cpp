23f8efb32ef3f8c045d7e23f151e299d94e3114d
Timothy Kindervatter <timothy@picon.kobol.net>
2025-08-06 12:49:44 -0400

    Renamed more variables

--- src/gas_transmittance_impl.cpp
+++ src/gas_transmittance_impl.cpp
@@ -47,16 +47,16 @@ void ozone_transmittance(L1_Record* l1_rec, Transmittance_Record* t_rec, bool do
 
         for (int iw = 0; iw < l1_rec->num_wavelengths; iw++) 
         {
-            double tau_oz = l1_rec->ozone_concentration[ip] * l1_rec->ozone_absorption_cross_section[iw];
-            t_rec->gas_transmittance_solar_zenith[row_offset + iw] = exp(-(tau_oz / l1_rec->cos_solar_zenith[ip]));
+            double ozone_optical_depth = l1_rec->ozone_concentration[ip] * l1_rec->ozone_absorption_cross_section[iw];
+            t_rec->gas_transmittance_solar_zenith[row_offset + iw] = exp(-(ozone_optical_depth / l1_rec->cos_solar_zenith[ip]));
 
             if (do_amf_correction) 
             {
-                t_rec->gas_transmittance_total[row_offset + iw] = exp(-tau_oz * (1.0/l1_rec->cos_solar_zenith[ip] + 1.0/l1_rec->cos_sensor_zenith[ip]));
+                t_rec->gas_transmittance_total[row_offset + iw] = exp(-ozone_optical_depth * (1.0/l1_rec->cos_solar_zenith[ip] + 1.0/l1_rec->cos_sensor_zenith[ip]));
             } 
             else 
             {
-                t_rec->gas_transmittance_sensor_zenith[row_offset + iw] = exp(-(tau_oz / l1_rec->cos_sensor_zenith[ip]));
+                t_rec->gas_transmittance_sensor_zenith[row_offset + iw] = exp(-(ozone_optical_depth / l1_rec->cos_sensor_zenith[ip]));
             }
         }
     }
@@ -70,23 +70,23 @@ void co_transmittance(L1_Record* l1_rec, Transmittance_Record* t_rec, bool do_am
     {
         int row_offset = ip*l1_rec->num_wavelengths; // Each row represents a single pixel and has num_wavelengths elements
 
-        double amf_solz = 1.0/l1_rec->cos_solar_zenith[ip];
-        double amf_senz = 1.0/l1_rec->cos_sensor_zenith[ip];
+        double amf_solar_zenith = 1.0/l1_rec->cos_solar_zenith[ip];
+        double amf_sensor_zenith = 1.0/l1_rec->cos_sensor_zenith[ip];
         
         for (int iw = 0; iw < l1_rec->num_wavelengths; iw++) 
         {
             if (do_amf_correction) 
             {
-                double amf_total = amf_solz + amf_senz;
-                int32_t index = iw*l1_rec->num_amf_grid_points;
+                double amf_total = amf_solar_zenith + amf_sensor_zenith;
+                int32_t row_index = iw*l1_rec->num_amf_grid_points;
 
-                t_rec->gas_transmittance_solar_zenith[row_offset + iw] = interpolate_transmittance_to_amf(l1_rec, l1_rec->co_transmittance, index, amf_solz);
-                t_rec->gas_transmittance_total[row_offset + iw] = interpolate_transmittance_to_amf(l1_rec, l1_rec->co_transmittance, index, amf_total);
+                t_rec->gas_transmittance_solar_zenith[row_offset + iw] = interpolate_transmittance_to_amf(l1_rec, l1_rec->co_transmittance, row_index, amf_solar_zenith);
+                t_rec->gas_transmittance_total[row_offset + iw] = interpolate_transmittance_to_amf(l1_rec, l1_rec->co_transmittance, row_index, amf_total);
             }
             else 
             {
-                t_rec->gas_transmittance_solar_zenith[row_offset + iw] = pow(l1_rec->co_transmittance[iw], amf_solz);
-                t_rec->gas_transmittance_sensor_zenith[row_offset + iw] = pow(l1_rec->co_transmittance[iw], amf_senz);
+                t_rec->gas_transmittance_solar_zenith[row_offset + iw] = pow(l1_rec->co_transmittance[iw], amf_solar_zenith);
+                t_rec->gas_transmittance_sensor_zenith[row_offset + iw] = pow(l1_rec->co_transmittance[iw], amf_sensor_zenith);
             }
         }
     }
@@ -100,14 +100,14 @@ void no2_transmittance(L1_Record* l1_rec, Transmittance_Record* t_rec, bool do_a
     {
         double sec0 = 1.0 / l1_rec->cos_solar_zenith[ip];
         double sec = 1.0 / l1_rec->cos_sensor_zenith[ip];
-        double no2_tr200{0.0};
+        double tropospheric_no2_concentration_above_200m{0.0};
 
         if (l1_rec->tropospheric_no2_concentration[ip] > 0.0)
         {
             /* compute tropo no2 above 200m (Z.Ahmad)
-            no2_tr200 = exp(12.6615 + 0.61676*log(no2_tropo));
+            tropospheric_no2_concentration_above_200m = exp(12.6615 + 0.61676*log(no2_tropo));
             new, location-dependent method */
-            no2_tr200 = l1_rec->fraction_tropospheric_no2_above_200m[ip] * l1_rec->tropospheric_no2_concentration[ip];
+            tropospheric_no2_concentration_above_200m = l1_rec->fraction_tropospheric_no2_above_200m[ip] * l1_rec->tropospheric_no2_concentration[ip];
         }
 
         int row_offset = ip*l1_rec->num_wavelengths; // Each row represents a single pixel and has num_wavelengths elements
@@ -119,17 +119,18 @@ void no2_transmittance(L1_Record* l1_rec, Transmittance_Record* t_rec, bool do_a
                 double a_285 = l1_rec->no2_absorption_cross_section[iw] * (1.0 - 0.003 * (285.0 - 294.0));
                 double a_225 = l1_rec->no2_absorption_cross_section[iw] * (1.0 - 0.003 * (225.0 - 294.0));
 
-                double tau_to200 = a_285 * no2_tr200 + a_225 * l1_rec->stratospheric_no2_concentration[ip];
+                double no2_optical_depth_to_200m = a_285 * tropospheric_no2_concentration_above_200m 
+                                                 + a_225 * l1_rec->stratospheric_no2_concentration[ip];
 
-                t_rec->gas_transmittance_solar_zenith[row_offset + iw] = exp(-(tau_to200 * sec0));
+                t_rec->gas_transmittance_solar_zenith[row_offset + iw] = exp(-(no2_optical_depth_to_200m * sec0));
 
                 if (do_amf_correction) 
                 {
-                    t_rec->gas_transmittance_total[row_offset + iw] = exp(-(tau_to200 * (sec + sec0)));
+                    t_rec->gas_transmittance_total[row_offset + iw] = exp(-(no2_optical_depth_to_200m * (sec + sec0)));
                 }
                 else
                 {
-                    t_rec->gas_transmittance_sensor_zenith[row_offset + iw] = exp(-(tau_to200 * sec));
+                    t_rec->gas_transmittance_sensor_zenith[row_offset + iw] = exp(-(no2_optical_depth_to_200m * sec));
                 }
             }
         }
