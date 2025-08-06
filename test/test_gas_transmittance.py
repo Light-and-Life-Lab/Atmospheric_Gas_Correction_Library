ec17fe4846199df860fd529df196d58e92ec1339
Timothy Kindervatter <timothy@picon.kobol.net>
2025-08-06 10:48:13 -0400

    Minor changes to ozone and NO2 tests to reduce redundant code

--- test/test_gas_transmittance.py
+++ test/test_gas_transmittance.py
@@ -2,18 +2,13 @@ import numpy as np
 
 import bin.gas_transmittance as gas_transmittance
 
-def test_ozone_transmittance_struct():
-    k_oz = np.load('test/ozone_data/koz.npy')
-    l1b_oz = np.load('test/ozone_data/oz_concentration.npy')
-    l1b_csolz = np.load('test/ozone_data/oz_csolz.npy')
-    l1b_csenz = np.load('test/ozone_data/oz_csenz.npy')
-    do_amf_correction = False
-
+def test_ozone_transmittance():
     l1_rec = gas_transmittance.L1_Record()
-    l1_rec.k_oz = k_oz
-    l1_rec.l1b_oz = l1b_oz
-    l1_rec.l1b_csolz = l1b_csolz
-    l1_rec.l1b_csenz = l1b_csenz
+    l1_rec.k_oz = np.load('test/ozone_data/koz.npy')
+    l1_rec.l1b_oz = np.load('test/ozone_data/oz_concentration.npy')
+    l1_rec.l1b_csolz = np.load('test/ozone_data/oz_csolz.npy')
+    l1_rec.l1b_csenz = np.load('test/ozone_data/oz_csenz.npy')
+    do_amf_correction = False
 
     t_rec = gas_transmittance.ozone_transmittance(l1_rec, do_amf_correction)
     tg_sol_benchmark = np.load('test/ozone_data/tg_sol_oz.npy')
@@ -24,21 +19,14 @@ def test_ozone_transmittance_struct():
 
 
 def test_no2_transmittance():
-    k_no2 = np.load('test/no2_data/k_no2.npy')
-    l1b_no2_tropo = np.load('test/no2_data/no2_tropo.npy')
-    l1b_no2_strat = np.load('test/no2_data/no2_strat.npy')
-    l1b_no2_frac = np.load('test/no2_data/no2_frac.npy')
-    l1b_csolz = np.load('test/no2_data/no2_csolz.npy')
-    l1b_csenz = np.load('test/no2_data/no2_csenz.npy')
-    do_amf_correction = False
-
     l1_rec = gas_transmittance.L1_Record()
-    l1_rec.k_no2 = k_no2
-    l1_rec.l1b_no2_frac = l1b_no2_frac
-    l1_rec.l1b_no2_tropo = l1b_no2_tropo
-    l1_rec.l1b_no2_strat = l1b_no2_strat
-    l1_rec.l1b_csolz = l1b_csolz
-    l1_rec.l1b_csenz = l1b_csenz
+    l1_rec.k_no2 = np.load('test/no2_data/k_no2.npy')
+    l1_rec.l1b_no2_frac = np.load('test/no2_data/no2_frac.npy')
+    l1_rec.l1b_no2_tropo = np.load('test/no2_data/no2_tropo.npy')
+    l1_rec.l1b_no2_strat = np.load('test/no2_data/no2_strat.npy')
+    l1_rec.l1b_csolz = np.load('test/no2_data/no2_csolz.npy')
+    l1_rec.l1b_csenz = np.load('test/no2_data/no2_csenz.npy')
+    do_amf_correction = False
 
     t_rec = gas_transmittance.no2_transmittance(l1_rec, do_amf_correction)
     tg_sol_benchmark = np.load('test/no2_data/tg_sol_no2.npy')
