1135c639ef2fb9332ca5dd9eccbf12b4d9006860
TimKindervatter <kindert1@tcnj.edu>
2026-08-04 18:30:01 -0400

    Added additional assersions and keyword validation to python wrapper functions

--- gas_corrections_lib/src/gas_corrections.py
+++ gas_corrections_lib/src/gas_corrections.py
@@ -18,9 +18,15 @@ def Oxygen_A_Band_Option():
 
 
 def validate_keyword_args(args):
-        for key, value in args.items():
-            if value is None:
-                raise ValueError(f"Keyword argument {key} cannot have value {value}. Please populate the {key} variable and pass it as a keyword argument.")
+    for key, value in args.items():
+        if value is None:
+            raise ValueError(f"Keyword argument {key} cannot have value {value}. Please populate the {key} variable and pass it as a keyword argument.")
+        
+
+def check_for_invalid_keyword_args(args, valid_kwargs):
+    for key in args.keys():
+        if key not in valid_kwargs:
+            raise ValueError(f"{key} is not a valid keyword argument in this context.")
 
 
 class Gas_Correction_Manager:
@@ -292,6 +298,9 @@ def ozone_transmittance(**kwargs):
         gas_transmittances = gas_corrections.ozone_transmittance(l1_data=l1_data, \
                                                                  ancillary_data=ancillary_data)
     """
+    valid_kwargs = ['l1_data', 'ancillary_data']
+    check_for_invalid_keyword_args(kwargs, valid_kwargs)
+
     args = dict()
     l1_data = args['l1_data'] = kwargs.get("l1_data", None)
     ancillary_data = args['ancillary_data'] = kwargs.get("ancillary_data", None)
@@ -349,10 +358,13 @@ def co2_transmittance(**kwargs):
                                                                gas_transmittance_table=gas_transmittance_table, \
                                                                use_gas_transmittance_lookup_table=True)
     """
+    valid_kwargs = ['l1_data', 'gas_transmittance_table', 'use_gas_transmittance_lookup_table']
+    check_for_invalid_keyword_args(kwargs, valid_kwargs)
+
     args = dict()
     l1_data = args['l1_data'] = kwargs.get("l1_data", None)
     gas_transmittance_table = args['gas_transmittance_table'] = kwargs.get("gas_transmittance_table", None)
-    use_gas_transmittance_lookup_table = kwargs.get("use_gas_transmittance_lookup_table", True)
+    use_gas_transmittance_lookup_table = args['use_gas_transmittance_lookup_table'] = kwargs.get("use_gas_transmittance_lookup_table", True)
 
     validate_keyword_args(args)
 
@@ -413,10 +425,13 @@ def co_transmittance(**kwargs):
                                                               gas_transmittance_table=gas_transmittance_table, \
                                                               use_gas_transmittance_lookup_table=True)
     """
+    valid_kwargs = ['l1_data', 'gas_transmittance_table', 'use_gas_transmittance_lookup_table']
+    check_for_invalid_keyword_args(kwargs, valid_kwargs)
+
     args = dict()
     l1_data = args['l1_data'] = kwargs.get("l1_data", None)
     gas_transmittance_table = args['gas_transmittance_table'] = kwargs.get("gas_transmittance_table", None)
-    use_gas_transmittance_lookup_table = kwargs.get("use_gas_transmittance_lookup_table", True)
+    use_gas_transmittance_lookup_table = args['use_gas_transmittance_lookup_table'] = kwargs.get("use_gas_transmittance_lookup_table", True)
 
     validate_keyword_args(args)
 
@@ -477,10 +492,13 @@ def ch4_transmittance(**kwargs):
                                                                gas_transmittance_table=gas_transmittance_table, \
                                                                use_gas_transmittance_lookup_table=True)
     """
+    valid_kwargs = ['l1_data', 'gas_transmittance_table', 'use_gas_transmittance_lookup_table']
+    check_for_invalid_keyword_args(kwargs, valid_kwargs)
+
     args = dict()
     l1_data = args['l1_data'] = kwargs.get("l1_data", None)
     gas_transmittance_table = args['gas_transmittance_table'] = kwargs.get("gas_transmittance_table", None)
-    use_gas_transmittance_lookup_table = kwargs.get("use_gas_transmittance_lookup_table", True)
+    use_gas_transmittance_lookup_table = args['use_gas_transmittance_lookup_table'] = kwargs.get("use_gas_transmittance_lookup_table", True)
 
     validate_keyword_args(args)
 
@@ -541,10 +559,13 @@ def n2o_transmittance(**kwargs):
                                                                gas_transmittance_table=gas_transmittance_table, \
                                                                use_gas_transmittance_lookup_table=True)
     """
+    valid_kwargs = ['l1_data', 'gas_transmittance_table', 'use_gas_transmittance_lookup_table']
+    check_for_invalid_keyword_args(kwargs, valid_kwargs)
+
     args = dict()
     l1_data = args['l1_data'] = kwargs.get("l1_data", None)
     gas_transmittance_table = args['gas_transmittance_table'] = kwargs.get("gas_transmittance_table", None)
-    use_gas_transmittance_lookup_table = kwargs.get("use_gas_transmittance_lookup_table", True)
+    use_gas_transmittance_lookup_table = args['use_gas_transmittance_lookup_table'] = kwargs.get("use_gas_transmittance_lookup_table", True)
 
     validate_keyword_args(args)
 
@@ -623,11 +644,33 @@ def no2_transmittance(**kwargs):
         gas_transmittances = gas_corrections.no2_transmittance(l1_data=l1_data, \
                                                                ancillary_data=ancillary_data)
     """
+    valid_kwargs = ['l1_data', 'ancillary_data']
+    check_for_invalid_keyword_args(kwargs, valid_kwargs)
+
     args = dict()
     l1_data = args['l1_data'] = kwargs.get("l1_data", None)
     ancillary_data = args['ancillary_data'] = kwargs.get("ancillary_data", None)
 
     validate_keyword_args(args)
+
+    assert(l1_data.cos_solar_zenith.size != 0)
+    assert(l1_data.cos_sensor_zenith.size != 0)
+    assert(ancillary_data.no2_absorption_cross_section.size != 0)
+    assert(ancillary_data.fraction_tropospheric_no2_above_200m.size != 0)
+    assert(ancillary_data.tropospheric_no2_concentration.size != 0)
+    assert(ancillary_data.stratospheric_no2_concentration.size != 0)
+
+    assert(l1_data.cos_solar_zenith.shape == l1_data.cos_sensor_zenith.shape)
+    assert(l1_data.cos_solar_zenith.shape == ancillary_data.fraction_tropospheric_no2_above_200m.shape)
+    assert(l1_data.cos_solar_zenith.shape == ancillary_data.tropospheric_no2_concentration.shape)
+    assert(l1_data.cos_solar_zenith.shape == ancillary_data.stratospheric_no2_concentration.shape)
+
+    assert(l1_data.num_pixels == l1_data.cos_solar_zenith.size)
+    assert(l1_data.num_pixels == l1_data.cos_sensor_zenith.size)
+    assert(l1_data.num_pixels == ancillary_data.fraction_tropospheric_no2_above_200m.size)
+    assert(l1_data.num_pixels == ancillary_data.tropospheric_no2_concentration.size)
+    assert(l1_data.num_pixels == ancillary_data.stratospheric_no2_concentration.size)
+    assert(l1_data.num_wavelengths == ancillary_data.no2_absorption_cross_section.size)
 
     return gas_transmittance.no2_transmittance(l1_data, ancillary_data)
 
@@ -675,11 +718,14 @@ def o2_transmittance(**kwargs):
                                                               oxygen_A_band_option=gas_corrections.Oxygen_A_Band_Option.TRANSMITTANCE_TABLE, \
                                                               use_gas_transmittance_lookup_table=True)
     """
+    valid_kwargs = ['l1_data', 'gas_transmittance_table', 'oxygen_A_band_option', 'use_gas_transmittance_lookup_table']
+    check_for_invalid_keyword_args(kwargs, valid_kwargs)
+
     args = dict()
     l1_data = args['l1_data'] = kwargs.get("l1_data", None)
     gas_transmittance_table = args['gas_transmittance_table'] = kwargs.get("gas_transmittance_table", None)
     oxygen_A_band_option = kwargs.get("oxygen_A_band_option", gas_transmittance.Oxygen_A_Band_Option.TRANSMITTANCE_TABLE)
-    use_gas_transmittance_lookup_table = kwargs.get("use_gas_transmittance_lookup_table", True)
+    use_gas_transmittance_lookup_table = args['use_gas_transmittance_lookup_table'] = kwargs.get("use_gas_transmittance_lookup_table", True)
 
     validate_keyword_args(args)
 
@@ -687,6 +733,17 @@ def o2_transmittance(**kwargs):
     o2_transmittance_sensor_wavelengths = f(l1_data.wavelengths)
 
     gas_transmittance_table.o2_transmittance = o2_transmittance_sensor_wavelengths
+
+    assert(l1_data.cos_solar_zenith.size != 0)
+    assert(l1_data.cos_sensor_zenith.size != 0)
+    assert(l1_data.wavelengths.size != 0)
+    assert(gas_transmittance_table.o2_transmittance.size != 0)
+
+    assert(l1_data.cos_solar_zenith.shape == l1_data.cos_sensor_zenith.shape)
+    assert(l1_data.num_pixels == l1_data.cos_solar_zenith.size)
+    assert(l1_data.num_pixels == l1_data.cos_sensor_zenith.size)
+    assert(l1_data.num_wavelengths == l1_data.wavelengths.size)
+    assert(l1_data.num_wavelengths == gas_transmittance_table.o2_transmittance.shape[0])
 
     return gas_transmittance.o2_transmittance(l1_data, gas_transmittance_table, use_gas_transmittance_lookup_table, oxygen_A_band_option)
 
@@ -727,10 +784,13 @@ def h2o_transmittance(**kwargs):
                                                               gas_transmittance_table=gas_transmittance_table, \
                                                               use_gas_transmittance_lookup_table=True)
     """
+    valid_kwargs = ['l1_data', 'gas_transmittance_table', 'use_gas_transmittance_lookup_table']
+    check_for_invalid_keyword_args(kwargs, valid_kwargs)
+    
     args = dict()
     l1_data = args['l1_data'] = kwargs.get("l1_data", None)
     gas_transmittance_table = args['gas_transmittance_table'] = kwargs.get("gas_transmittance_table", None)
-    use_gas_transmittance_lookup_table = kwargs.get("use_gas_transmittance_lookup_table", True)
+    use_gas_transmittance_lookup_table = args['use_gas_transmittance_lookup_table'] = kwargs.get("use_gas_transmittance_lookup_table", True)
 
     validate_keyword_args(args)
 
@@ -744,6 +804,17 @@ def h2o_transmittance(**kwargs):
     h2o_transmittance_at_sensor_wavelengths = f(l1_data.wavelengths)
 
     gas_transmittance_table.num_wavelengths = len(l1_data.wavelengths)
-    gas_transmittance_table.h2o_transmittance = h2o_transmittance_at_sensor_wavelengths        
+    gas_transmittance_table.h2o_transmittance = h2o_transmittance_at_sensor_wavelengths
+
+    assert(l1_data.cos_solar_zenith.size != 0)
+    assert(l1_data.cos_sensor_zenith.size != 0)
+    assert(l1_data.wavelengths.size != 0)
+    assert(gas_transmittance_table.h2o_transmittance.size != 0)
+
+    assert(l1_data.cos_solar_zenith.shape == l1_data.cos_sensor_zenith.shape)
+    assert(l1_data.num_pixels == l1_data.cos_solar_zenith.size)
+    assert(l1_data.num_pixels == l1_data.cos_sensor_zenith.size)
+    assert(l1_data.num_wavelengths == l1_data.wavelengths.size)
+    assert(l1_data.num_wavelengths == gas_transmittance_table.h2o_transmittance.shape[1])    
 
     return gas_transmittance.h2o_transmittance(l1_data, ancillary_data, gas_transmittance_table, use_gas_transmittance_lookup_table)
